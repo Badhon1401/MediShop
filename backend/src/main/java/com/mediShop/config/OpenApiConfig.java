dca8f9cc96e33a77ecd46b3ab8f42b1ef55f2cde
Rakibul1411 <bsse1411@iit.du.ac.bd>
2025-07-05 09:48:21 +0600

    update config folder's files

--- backend/src/main/java/com/mediShop/config/OpenApiConfig.java
+++ backend/src/main/java/com/mediShop/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.mediShop.medicine.config;
 
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,9 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Medical Inventory API")
                         .version("1.0.0")
-                        .description("API documentation for Medical Inventory Management System"));
+                        .description("API documentation for Medical Inventory Management System"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080/mediShop").description("Local server")
+                ));
     }
 }
