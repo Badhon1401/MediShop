7f7a1464360fc53e7fe0fd23b36a7fb836382f6d
Badhon1401 <bsse1401@iit.du.ac.bd>
2025-08-05 12:51:10 +0600

    missing user jpa repository has been added.

---
+++ backend/src/main/java/com/mediShop/user/infrastructure/persistence/repository/UserJpaRepository.java
@@ -0,0 +1,19 @@
+package com.mediShop.user.infrastructure.persistence.repository;
+
+import com.mediShop.user.infrastructure.persistence.entity.UserJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
+
+    Optional<UserJpaEntity> findByUserName(String username);
+    @Override
+    Optional<UserJpaEntity> findById(UUID uuid);
+
+    List<UserJpaEntity> findByEmail(String email);
+}
